75bb95e19747c526969223e5b68e89b77fdf0f23
yitsenliu <l.yitsen@gmail.com>
2026-07-11 22:52:22 +0800

    更新Turnstile 網站金鑰

--- assets/121-config.js
+++ assets/121-config.js
@@ -1,5 +1,5 @@
 /* 部署 Cloudflare Worker 後，填入 Worker 網址與 Turnstile Site Key。請勿放入 GitHub Token。 */
 window.PTD121_CONFIG = {
-  workerUrl: "",
-  turnstileSiteKey: ""
+  workerUrl: "https://pt-d-proposal.l-yitsen.workers.dev",
+  turnstileSiteKey: "0x4AAAAAADz37GpQP0JNAd1P"
 };
